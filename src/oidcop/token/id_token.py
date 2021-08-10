bbec1d7667853e06d192b59126cf7e62bfa9892d
Nikos Sklikas <nsklikas@admin.grnet.gr>
2021-08-10 12:09:14 +0300

    Pass usage_rules to token handler

--- src/oidcop/token/id_token.py
+++ src/oidcop/token/id_token.py
@@ -267,6 +267,7 @@ class IDToken(Token):
         encrypt=False,
         code=None,
         access_token=None,
+        usage_rules=None,
         **kwargs,
     ) -> str:
         _context = self.server_get("endpoint_context")
